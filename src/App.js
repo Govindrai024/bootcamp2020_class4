bca95d1acd6a47708fc035ecc904e59641177923
Govindrai024 <govindrai024@gmail.com>
2020-06-19 02:03:05 +0500

    Deploying code

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { Cart } from './cart.js';
+import './App.css';
+
+// Simple counter with Using Hooks.
+
+export default function App() {
+
+  let [Count, setCount] = useState(0);
+  let [ischartFull, setCharFull] = useState(true)
+
+
+  return (
+    <div className={"header ${ ischartFull ? 'daylight' : ''}"}>
+      <div>
+        <h3>Cart is: {ischartFull ? 'Full' : 'Empty'}</h3>
+
+        <button onClick={() => setCharFull(!ischartFull)}>Check Cart</button>
+
+      </div>
+
+      <Cart counter={Count} />
+
+      <div>
+        <button onClick={() => setCount(++Count)}>Increase Quantity</button>
+        <button onClick={() => setCount(--Count)}>Decrease Quantity</button>
+      </div>
+
+    </div>
+
+  );
+}
+
+// export default App;
---
+++ src/cart.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+export function Cart (props) {
+
+    return(
+            <h3>Select your quantity: {props.counter}</h3>
+    )
+}
